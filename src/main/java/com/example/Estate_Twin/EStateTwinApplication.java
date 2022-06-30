c7a345bc0c2a28f7b91cfceebaee0bc961c63fb1
Cho Soyeon <sophia5460@naver.com>
2022-06-30 14:44:16 +0900

    first commit

---
+++ src/main/java/com/example/Estate_Twin/EStateTwinApplication.java
@@ -0,0 +1,13 @@
+package com.example.Estate_Twin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStateTwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStateTwinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Estate_Twin/EStateTwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Estate_Twin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStateTwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
