7f6b82973b48ec7cad1d0ca991a8dfce16ac60ca
algosipdahack <sophia5460@ewhain.net>
2022-08-08 18:51:40 +0900

    ✅ test: upload file test

--- src/main/java/com/example/Estate_Twin/config/SecurityConfig.java
+++ src/main/java/com/example/Estate_Twin/config/SecurityConfig.java
@@ -4,15 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @Slf4j
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -30,14 +33,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
                 .antMatchers("/user").hasRole("USER")
                 .antMatchers("/manager").hasRole("MANAGER")
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers(
                         "/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**",
-                        "/h2-console/**", "/favicion.ico"
+                        "/h2-console/**", "/favicion.ico", "api/estate/detail/**", "/upload"
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and()
--- src/main/java/com/example/Estate_Twin/media/domain/Media.java
+++ src/main/java/com/example/Estate_Twin/media/domain/Media.java
@@ -17,7 +17,7 @@ public class Media {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "media_id")
-    private Long id;
+    private int id;
 
     @Column
     private String origFileName;
