3ade3cb7c68a4469351f711fc61610e353fa616b
algosipdahack <sophia5460@ewhain.net>
2022-09-03 21:35:23 +0900

    🔒️ fix: fix CORS error

--- src/main/java/com/example/Estate_Twin/config/WebConfig.java
+++ src/main/java/com/example/Estate_Twin/config/WebConfig.java
@@ -2,15 +2,18 @@ package com.example.Estate_Twin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET","POST","PUT")
+                .allowedHeaders("*")
+                .allowedMethods("*")
                 .maxAge(3600);
     }
 }
