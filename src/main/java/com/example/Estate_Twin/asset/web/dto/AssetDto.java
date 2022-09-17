032ed0edbb08e4ebf6f9a1287344afec5d80584a
algosipdahack <sophia5460@ewhain.net>
2022-09-17 21:29:49 +0900

    ➖ refactor: dependency제거

--- src/main/java/com/example/Estate_Twin/asset/web/dto/AssetDto.java
+++ src/main/java/com/example/Estate_Twin/asset/web/dto/AssetDto.java
@@ -3,15 +3,16 @@ package com.example.Estate_Twin.asset.web.dto;
 import com.example.Estate_Twin.asset.data.entity.Asset;
 import com.example.Estate_Twin.checklist.data.entity.Category;
 import com.example.Estate_Twin.estate.web.dto.EstateResponseDto;
-import com.example.Estate_Twin.media.domain.entity.Media;
+import com.example.Estate_Twin.media.web.dto.MediaResponseDto;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 @Getter
 public class AssetDto {
     private final EstateResponseDto estate;
     private final Category category;
-    private final List<Media> assetPhoto;
+    private final List<MediaResponseDto> assetPhoto;
     private final String assetName;
     private final String productName;
     private final String manufacturer;
@@ -19,7 +20,8 @@ public class AssetDto {
     public AssetDto(Asset asset) {
         this.estate = new EstateResponseDto(asset.getEstate());
         this.category = asset.getCategory();
-        this.assetPhoto = asset.getAssetPhoto();
+        this.assetPhoto = new ArrayList<>();
+        asset.getAssetPhoto().forEach(photo -> assetPhoto.add(new MediaResponseDto(photo)));
         this.assetName = asset.getAssetName();
         this.productName = asset.getProductName();
         this.manufacturer = asset.getManufacturer();
