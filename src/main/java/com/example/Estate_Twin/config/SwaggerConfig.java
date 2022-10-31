5b1af3693ce38fe823376d43ff606f2f847f876d
algosipdahack <sophia5460@ewhain.net>
2022-10-31 16:05:44 +0900

    ✨feat: add swagger jwt token

--- src/main/java/com/example/Estate_Twin/config/SwaggerConfig.java
+++ src/main/java/com/example/Estate_Twin/config/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package com.example.Estate_Twin.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.*;
 
+import java.util.Arrays;
 @Configuration
 public class SwaggerConfig {
 
@@ -16,11 +19,24 @@ public class SwaggerConfig {
                 .pathsToMatch("/api/**")
                 .build();
     }
+
+    io.swagger.v3.oas.models.security.SecurityScheme securityScheme =
+            new io.swagger.v3.oas.models.security.SecurityScheme()
+                    .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+                    .in(io.swagger.v3.oas.models.security.SecurityScheme.In.HEADER)
+                    .name("X-AUTH-TOKEN");
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("Idiot API")
                         .description("E-state Twin 프로젝트 API 명세서입니다.")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
+                .security(Arrays.asList(securityRequirement));
     }
 }
+
